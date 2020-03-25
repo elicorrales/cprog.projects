a1ef1f073a16dc92fcd219feaaf9be4860270128
devchu <devchu@standalone-debian-vm.vm.standalone.com>
2020-03-24 21:02:29 -0400

    .

---
+++ usb.master/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <termios.h>
+
+
+int main() {
+
+    ////////////////////////////////////////////////////////
+    // open port
+    ////////////////////////////////////////////////////////
+    int serial_port = open("/dev/ttyACM0", O_RDWR | O_NOCTTY | O_NDELAY);
+    if (serial_port < 0) {
+        printf("Error %i from open : %s\n", errno, strerror(errno));
+        return -1;
+    }
+
+    ////////////////////////////////////////////////////////
+    // is it a tty?
+    ////////////////////////////////////////////////////////
+    if (!isatty(serial_port)) {
+        printf("Is not a tty\n");
+        return -1;
+    }
+
+    ////////////////////////////////////////////////////////
+    // get current port settings
+    ////////////////////////////////////////////////////////
+    struct termios tty;
+    memset(&tty, 0, sizeof(tty));
+    if (tcgetattr(serial_port, &tty) != 0) {
+        printf("Error %i from gett attr : %s\n", errno, strerror(errno));
+        return -1;
+    }
+
+
+    char buffer[1] = {0};
+
+    while ( 1 ) {
+        int num = read(serial_port, buffer, sizeof(buffer));
+        if (num < 0) {
+            perror("The error is:");
+        } else if (num > 0) {
+            printf("%c",buffer);
+        } else {
+            printf(".");
+        }
+    }    
+
+
+
+
+    return 0;
+}
